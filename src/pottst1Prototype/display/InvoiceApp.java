f37c39c955d8f61c0dbd621e2efc19bbb4a03df4
Musicachic <Musicachic@gmail.com>
2014-02-09 16:01:03 -0500

    Added java docs to the program as required.

--- src/pottst1Prototype/display/InvoiceApp.java
+++ src/pottst1Prototype/display/InvoiceApp.java
@@ -18,12 +18,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import pottst1Prototype.display.InvoiceDisplay;
 
+/**
+ * This is the class that creates the invoice array list. It validates that
+ * the UPC entered is in the ProductList class and then prompts for the amount
+ * desired. Utimately it should also take the quantity based on what is
+ * available.
+ */
 public class InvoiceApp
 {
 	public static ArrayList<Product> invoice = new ArrayList<>();
 	private static int invoiceSize = 0;
 	public int quantityPurchased = 0;
 
+	/**
+	 * This method is used to add products to the invoice arrayList. It
+	 * checks to see if the UPC exists in the ProductList and if does adds it
+	 * to the arrayList.
+	 */
 	public static void addDisplay()
 	{
 
@@ -60,12 +71,22 @@ public class InvoiceApp
 
 	}
 
+	/**
+	 * This method as the user how many of a product a customer will be
+	 * purchasing.
+	 */
 	public static void askForQuantity(){
 
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the quantity for purchase: ");
 	}
 
+	/**
+	 * This is the getter for the invoice total based on all the items added
+	 * or removed from the invoice.
+	 * @return invoiceTotal This is the total price of all products on the
+	 * invoice.
+	 */
 	public BigDecimal getInvoiceTotal()
 	{
 
@@ -77,6 +98,10 @@ public class InvoiceApp
 		return invoiceTotal;
 	}
 
+	/**
+	 * This formats the invoice total to currency format for readability.
+	 * @return getInvoiceTotal in a readable currency format.
+	 */
 	public String getFormattedTotal()
 	{
 		NumberFormat currency = NumberFormat.getCurrencyInstance();
