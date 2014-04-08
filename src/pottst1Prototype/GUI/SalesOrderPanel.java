18ce4fb20f43f74bd59ea148d950a403929dfb7b
Musicachic <Musicachic@gmail.com>
2014-04-07 23:32:43 -0400

    eh...

--- src/pottst1Prototype/GUI/SalesOrderPanel.java
+++ src/pottst1Prototype/GUI/SalesOrderPanel.java
@@ -16,6 +16,9 @@ import java.awt.*;
 public class SalesOrderPanel extends JPanel
 {
 
+	private JLabel errorMessage = new JLabel("");
+	private JFrame frame;
+
 	public SalesOrderPanel()
 
 	{
@@ -27,38 +30,47 @@ public class SalesOrderPanel extends JPanel
 		//Add button section
 		JPanel buttonPanel = new JPanel();
 		JButton addButton = new JButton("Add");
-		addButton.addActionListener(new SalesOrderPanelListener());
+		addButton.addActionListener(new SalesOrderPanelListener(this));
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		buttonPanel.add(addButton);
 		this.add(buttonPanel);
 
 		buttonPanel = new JPanel();
 		JButton removeButton = new JButton("Remove");
-		removeButton.addActionListener(new SalesOrderPanelListener());
+		removeButton.addActionListener(new SalesOrderPanelListener(this));
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		buttonPanel.add(removeButton);
 		this.add(buttonPanel);
 
 		buttonPanel = new JPanel();
 		JButton finishButton = new JButton("Finish");
-		finishButton.addActionListener(new SalesOrderPanelListener());
+		finishButton.addActionListener(new SalesOrderPanelListener(this));
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		buttonPanel.add(finishButton);
 		this.add(buttonPanel);
 
 		buttonPanel = new JPanel();
 		JButton managementButton = new JButton("Management");
-		managementButton.addActionListener(new SalesOrderPanelListener());
+		managementButton.addActionListener(new SalesOrderPanelListener(this));
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		buttonPanel.add(managementButton);
 		this.add(buttonPanel);
 
 		buttonPanel = new JPanel();
 		JButton exitButton = new JButton("Exit");
-		exitButton.addActionListener(new SalesOrderPanelListener());
+		exitButton.addActionListener(new SalesOrderPanelListener(this));
 		buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 		buttonPanel.add(exitButton);
 		this.add(buttonPanel);
+	}
+
+	public void displayError(String message) {
+		errorMessage.setText(message);
+	}
+
+	public JFrame getFrame()
+	{
+		return frame;
 	}
 
 }
--- src/pottst1Prototype/GUI/SalesOrderPanelListener.java
+++ src/pottst1Prototype/GUI/SalesOrderPanelListener.java
@@ -22,8 +22,9 @@ public class SalesOrderPanelListener implements ActionListener
 	private JButton finish;
 	private JButton management;
 	private JButton exit;
+	public SalesOrderPanel initialSalesDisplay;
 
-	public SalesOrderPanelListener()
+	public SalesOrderPanelListener(JButton add, JButton remove, JButton finish, JButton management, JButton exit)
 	{
 		this.add = add;
 		this.remove = remove;
@@ -32,13 +33,24 @@ public class SalesOrderPanelListener implements ActionListener
 		this.exit = exit;
 	}
 
+	public SalesOrderPanelListener(SalesOrderPanel salesOrderPanel)
+	{
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
 		System.out.println("Button clicked");
 
+		JButton source = (JButton) e.getSource();
+		if(add.getText().equals(this)){
+			source.setText("Please wait...");
+			source.setEnabled(false);
+			initialSalesDisplay.displayError("Successful");
+		}
+
 		if (e.getSource() instanceof JButton){
-			JButton source = (JButton) e.getSource();
+			//source = (JButton) e.getSource();
 
 			source.setText("Please wait...");
 			source.setEnabled(false);
